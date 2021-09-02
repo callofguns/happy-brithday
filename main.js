e7fcc992aeff12ecd06abc2ee04803b4d804371c
callofguns <84049001+callofguns@users.noreply.github.com>
2021-09-02 18:04:09 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,21 +1,32 @@
 var canvas = new fabric.Canvas('myCanvas');
  var x= document.getElementById("myAudio");
+var block_Img_object = "";
 
 function new_image()
 {
-    fabric.Image.fromURL('BirthdayImage.jpg',Img);
+    fabric.Image.fromURL('BirthdayImage.jpg',function(Img){
+
     block_Img_object = Img;
 
-    block_Img_object.scaleToWidth(750);
+    block_Img_object.scaleToWidth(700);
     block_Img_object.scaleToHeight(510);
     block_Img_object.set({
     top:0,
     left:0
     });
     canvas.add( block_Img_object);
-    };
-{}
+    });
+}
 
 function playSound(){
 	x.play();
+}
+window.addEventListener("keydown",myKeyDown);
+
+function myKeyDown(e){
+    Keypress = e.keyCode;
+
+    if(Keypress == '38'){
+        new_image();
+    }
 }
